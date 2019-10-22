89cc9d924b2d483bbd1f5bc90cf37c1e39634c0d
TakaraTamura <shonan.rgg@gmail.com>
2019-10-22 20:21:26 +0900

    マジックナンバーの削除

--- homography.py
+++ homography.py
@@ -8,7 +8,6 @@ Created on Thu Sep  5 18:24:17 2019
 
 import cv2
 import numpy as np
-from PIL import Image
 
 from IPython.display import display, Image
 
@@ -16,49 +15,36 @@ def display_cv_image(image, formats='.png'):
     decoded_bytes = cv2.imencode(formats, image)[1].tobytes()
     display(Image(data=decoded_bytes))
     
-def homography(name):
-    img = cv2.imread(name)
-    img = cv2.resize(img, (512, 512))
-    #display_cv_image(img)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    ret, th1 = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
-    #display_cv_image(th1)
-
-    contours, hierarchy = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+def homography(path, input_img_width, input_img_height, area, area_color, out_img_width, out_img_height):
+    
+    img = cv2.imread(path)
+    img = cv2.resize(img, (input_img_width, input_img_height))
     areas = []
-    """
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 10000:
-            epsilon = 0.1*cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, epsilon, True)
-            areas.append(approx)
-    """
-    areas.append(np.array([[[492, 400], [488, 107], [125, 25], [124, 512]]]))
-    cv2.drawContours(img, areas[0], -1, (0, 0, 255), 3)
+    
+    areas.append(area)
+    cv2.drawContours(img, areas[0], -1, area_color, 3)
     display_cv_image(img)
     print(areas[0])
-    img = cv2.imread(name)
-    img = cv2.resize(img, (512, 512))
+
     dst = []
 
     pts1 = np.float32(areas[0])
-    pts2 = np.float32([[650,366],[650,0],[0,0],[0,366]])
+    pts2 = np.float32([[out_img_width, out_img_height],[out_img_width,0],[0,0],[0,out_img_height]])
 
     M = cv2.getPerspectiveTransform(pts1,pts2)
-    dst = cv2.warpPerspective(img,M,(650,366))
+    dst = cv2.warpPerspective(img,M,(out_img_width,out_img_height))
 
     display_cv_image(dst)
     return dst
 
-if __name__ == "__main__":
-    path = "./TestImage/test008/raw現像2/"
-    out = "./TestImage/test008/"
-    for d in range(120, 260, 10):
-        name = path + str(d) + ".png"
-        i = homography(name)
-        name = out + str(d) + ".png"
-        cv2.imwrite(name, i)
+if __name__ == '__main__':
+    path = "./test.png"
+    input_img_width = 512
+    input_img_height = 512
+    area = np.array([[[492, 400], [488, 107], [125, 25], [124, 512]]])
+    area_color = (0, 0, 255)
+    out_img_width = 650
+    out_img_height = 366
+    
+    homography(path, input_img_width, input_img_height, area, area_color, out_img_width, out_img_height)
+    
